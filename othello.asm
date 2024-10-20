ded2686c91f06c8e68a916441011079e7a961daf
Bogdan Stanciu <wowvain-dev@gmail.com>
2024-10-20 11:07:34 +0200

    Alot of progress. Started working on the input processing.

--- othello.asm
+++ othello.asm
@@ -3,8 +3,12 @@
 
 
 section .bss
-	board resb 64 
-	input resb 2
+	board 			resb 64 
+	input_buffer 	resb 2
+	current_move 	resb 1
+
+	white_score 	resb 1
+	black_score 	resb 1
 
 section .data
 	walls 			db "|         |         |         |         |         |         |         |         |", 0 	
@@ -21,7 +25,20 @@ section .data
 	w_count 		db "White pieces: %d", 0
 	keybinds 		db "Keybinds:", 0 
 	f1_help 		db "F1 - Help", 0
-	f2_about 		db "F2 - About", 0, 
+	f2_about 		db "F2 - About", 0
+	i_enter			db "I  - Enter Move", 0 
+	q_exit			db "Q  - Exit", 0
+
+	player			db "CURRENT PLAYER: %s", 0
+	player_black 	db "BLACK(@)", 0
+	player_white 	db "WHITE(o)", 0
+
+	sure			db "Are you sure you want to exit? [y/n]"
+	sure_end        db 0 ; used to get sure string length
+
+	input 			db "Enter your move (ex: a3, ONLY LOWERCASE, ESC to cancel): __", 0
+	input_end 		db 0 ; used to get input string length
+	bad_move_input	db "Bad Move Input, X coord = (a-h); y coord = (0-7). Enter to confirm", 0
 		
 
 section .text
@@ -33,6 +50,8 @@ section .text
     extern getch     ; NCurses function to wait for input
     extern endwin    ; NCurses function to end NCurses mode
 	extern mvprintw  
+	extern move
+	extern clrtoeol
 
 main:
 	push rbp
@@ -41,30 +60,13 @@ main:
 	; Initialize NCurses
     call initscr
 
+	; Clearing the board buffer
 	call clear_board
 
-	mov byte [board + (4 * 8 + 3)], 2
-
-    ; Draw the Othello board
-	call draw_board
-
-
-    ; Refresh the screen to show the board
-    call refresh
-
-	mov byte [board + (4 * 8 + 4)], 1
-	call draw_pieces
-
-	call refresh
-
-    ; Wait for a key press
-    call getch
-
-	;call game_loop
+	call game_loop
 
     ; End NCurses mode
     call endwin
-		
 	mov rsp, rbp
 	pop rbp
 
@@ -155,6 +157,24 @@ draw_board:
 		cmp r13, 8
 	jl draw_board.loop_coords
 
+	mov rdi, 38
+	mov rsi, 2
+	mov rdx, player
+	cmp byte [current_move], 0
+	je draw_board.black_move
+	cmp byte [current_move], 1
+	je draw_board.white_move
+	.black_move:
+		mov rcx, player_black
+		call mvprintw
+		jmp draw_board.go_pieces
+	.white_move:
+		mov rcx, player_white
+		call mvprintw
+		jmp draw_board.go_pieces
+
+	.go_pieces:
+
 	call draw_pieces		
 
 	pop r15
@@ -186,6 +206,8 @@ draw_pieces:
 		add r12, 1
 		cmp r12, 8	
 	jl .loop_rows
+
+	
 
 	call draw_menu
 
@@ -288,22 +310,32 @@ draw_menu:
 
 	mov rdi, 11
 	mov rsi, 92
-	mov rdx, f1_help
+	mov rdx, i_enter
 	call mvprintw
 
 	mov rdi, 12
 	mov rsi, 92
+	mov rdx, f1_help
+	call mvprintw
+
+	mov rdi, 13
+	mov rsi, 92
 	mov rdx, f2_about
 	call mvprintw
 
-	mov rdi, 15
+	mov rdi, 14
+	mov rsi, 92
+	mov rdx, q_exit
+	call mvprintw
+
+	mov rdi, 17
 	mov rsi, 90
 	mov rdx, score
 	call mvprintw
 
-	mov rcx, 0
-	mov r12, 0
-	mov r13, 0
+	xor rcx, rcx
+	xor r12, r12
+	xor r13, r13
 	.score_loop:
 		xor rdi, rdi
 		movzx rdi, byte [board + rcx]		
@@ -322,14 +354,17 @@ draw_menu:
 		add rcx, 1	
 		cmp rcx, 64
 	jl draw_menu.score_loop
+
+	mov [white_score], r12
+	mov [black_score], r13
 	
-	mov rdi, 16
+	mov rdi, 18
 	mov rsi, 92
 	mov rdx, w_count
 	mov rcx, r12
 	call mvprintw
 
-	mov rdi, 17
+	mov rdi, 19
 	mov rsi, 92
 	mov rdx, b_count
 	mov rcx, r13
@@ -343,4 +378,145 @@ draw_menu:
 	ret
 
 game_loop:
+	push rbp
+	mov rbp, rsp
 
+	; Initial board state
+	mov byte [board + (3*8+3)], 1
+	mov byte [board + (3*8+4)], 2
+	mov byte [board + (4*8+3)], 2
+	mov byte [board + (4*8+4)], 1
+
+	.loop:
+    	; Draw the Othello board
+		call draw_board
+
+    	; Refresh the screen to show the board
+    	call refresh
+
+		mov rdi, 39
+		mov rsi, 2
+		call move
+
+    	; Wait for a key press
+    	call getch
+
+		cmp rax, 73
+		je game_loop.input_move
+		cmp rax, 105
+		je game_loop.input_move
+
+		cmp rax, 81
+		je game_loop.ask_exit
+		cmp rax, 113
+		je game_loop.ask_exit
+	jmp game_loop.loop	
+
+	.input_move:
+	call get_input
+	jmp game_loop.loop
+
+	.ask_exit:
+	mov rdi, 40
+	mov rsi, 2
+	mov rdx, sure
+	call mvprintw
+
+	.repeat_exit:
+	mov rdi, 40
+	mov rsi, sure_end - sure
+	add rsi, 2
+	call move
+	call getch
+	
+	; y / Y
+	cmp rax, 121
+	je game_loop.exit
+	cmp rax, 089
+	je game_loop.exit
+	
+	; n / N
+	cmp rax, 110
+	jne game_loop.lower_n
+
+		mov rdi, 40
+		mov rsi, 0
+		call move
+		call clrtoeol
+
+	jmp game_loop.loop
+	
+	.lower_n:
+	cmp rax, 78
+	jne game_loop.repeat_exit
+
+		mov rdi, 40
+		mov rsi, 0
+		call move
+		call clrtoeol
+
+	jmp game_loop.loop
+
+	.exit:
+	mov rsp, rbp
+	pop rbp
+ret
+
+get_input:
+	push rbp
+	mov rbp, rsp
+
+	.retry:
+	mov rdi, 39
+	mov rsi, 0
+	call clrtoeol
+
+	mov rdi, 39
+	mov rsi, 2
+	mov rdx, input
+	call mvprintw
+
+	mov rdi, 39
+	mov rsi, input_end - input - 1
+	call move
+
+	call getch
+	cmp rax, 27
+	je get_input.exit
+	cmp rax, 97
+	jl get_input.bad_input
+	cmp rax, 104
+	jg get_input.bad_input
+
+	call getch
+	cmp rax, 27
+	je get_input.exit
+	cmp rax, 48
+	jl get_input.bad_input
+	cmp rax, 55
+	jg get_input.bad_input
+	
+	jmp get_input.good_input
+
+	.bad_input:
+		mov rdi, 39
+		mov rsi, 2
+		mov rdx, bad_move_input
+		call mvprintw
+		call getch
+		cmp rax, 10
+		je get_input.retry
+	jmp get_input.bad_input
+
+	.good_input:
+
+	
+	.exit:
+	mov rdi, 39
+	mov rsi, 0
+	call move
+	call clrtoeol
+
+	mov rsp, rbp
+	pop rbp
+ret
